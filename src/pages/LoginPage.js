2e7bc9a29500023c5e1b83e045d6ea61480a2d9a
Bianca Belinda <belindabee42@gmail.com>
2020-03-16 16:57:47 +0700

    [Binca]- livestock is ready to use

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Login from "../components/Auth/Login";
+
+export default class LoginPage extends Component {
+  render() {
+    return (
+      <div>
+        <Login />
+      </div>
+    );
+  }
+}
