152b0ae3b326cb740b4d11a61afad74d9d98519c
Adityo Nento <adityosnento11@gmail.com>
2020-03-16 15:03:02 +0700

    [Adityo S. Nento] - Livestock

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import './App.css'
-import Header from './components/Header/Header2'
-import Footer from './components/Footer/Footer'
-import Mainpage from './components/MainPage/Mainpage';
-
+import React from "react";
+import { connect } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header2";
+// import Footer from './components/Footer/Footer'
+import Login from "./components/Auth/Login/Login";
+import Register from "./components/Auth/Register/Register";
+import Routes from "./components/Routes/Routes";
 
 const App = () => {
-  
   return (
-         <div>
-          <Header/>
-          <Mainpage/>
-          {/* <Footer/> */}
-         </div>
-       
+    <Router>
+      <Header />
+      <Routes />
+      {/* <Footer/> */}
 
-  
+      <Route path="/login">
+        <Login />
+      </Route>
+
+      <Route path="/register">
+        <Register />
+      </Route>
+    </Router>
   );
-}
+};
 
-
-
-export default connect () (App);
+export default connect()(App);
