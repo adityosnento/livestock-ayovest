f531f5c54ee0eb3934d005571c3fd74cfdc1cfe4
Bianca Belinda <belindabee42@gmail.com>
2020-03-21 10:49:03 +0700

    ancur

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import Home from "./pages/Home";
 import "./asset/style.css";
 import FooterComponent from "./components/FooterComponent";
 import "react-toastify/dist/ReactToastify.min.css";
-import Payment from "./pages/Payment"
+import Payment from "./pages/Payment";
 
 import Detail from "./pages/Detail";
 import LiveeStock from "./pages/LiveeStock";
 import Profile from "./pages/Profile";
 import "react-toastify/dist/ReactToastify.min.css";
+import Breeders from "./pages/Breeders";
 
 function App() {
   return (
@@ -23,7 +24,8 @@ function App() {
           <Route path="/detail/:id" component={Detail} />
           <Route path="/livestock" component={LiveeStock} />
           <Route path="/profile/:id" component={Profile} />
-          <Route path="/payment" component={Payment}/>
+          <Route path="/payment" component={Payment} />
+          <Route path="/breeder" component={Breeders} />
         </Switch>
       </Router>
       <FooterComponent />
--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -99,3 +99,14 @@ export const updateUserDataInvestor = data => {
     url: "investors"
   });
 };
+
+/**
+ * Rrecovering password
+ */
+export const investorRecoverPassword = data => {
+  return axios({
+    method: "POST",
+    data: data,
+    url: "investors/recover"
+  });
+};
