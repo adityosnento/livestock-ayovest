394e198693a55c76f112bc23325f4fbc8ec475b9
Bianca Belinda <belindabee42@gmail.com>
2020-03-22 17:58:08 +0700

    payment design

--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -1,51 +1,65 @@
-import React from 'react';
-import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import '../payment/Payment.scss'
+import React from "react";
+import { Col, Row, Button, Form, FormGroup, Label, Input } from "reactstrap";
+import "../payment/Payment.scss";
 
-const FormPayments = (props) => {
+const FormPayments = props => {
   return (
     <div className="container__payment">
-    <div className="container__left">
-      <img src={require("../../asset/Payment/payment.svg")}
+      <div className="container__left">
+        {/* <img src={require("../../asset/Payment/payment.svg")}
         alt="logo" />
         <div className="container__content">
         <h5>Payment Method</h5>
         <p>Whether you are an enterpreneur, businessman, employment, or someone with an interest in farm industry.</p>
+        </div> */}
+        <h6>INVOICE</h6>
+        <h1>Bills ELLIES</h1>
+        <div className="payment__flex">
+          <div className="payment__content">
+            <p>Contract Period</p>
+            <p>Return Of Investment</p>
+            <p>Time Period</p>
+            <p>Total Lot</p>
+          </div>
+          <div className="payment__value">
+            <p>:  10 years</p>
+            <p>:  20% - 30%</p>
+            <p>:  After 4 years</p>
+            <p>:  10 Lot</p>
+          </div>
         </div>
-    </div>
-    <div className="container__right">
-    <Form>
-    <FormGroup>
-        <Label for="exampleAddress">Payment Method</Label>
-        <Input type="text" name="address" id="exampleAddress" placeholder="Transfer Bank"/>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleAddress">Card Number</Label>
-        <Input type="text" name="address" id="exampleAddress" placeholder="0000 0000 0000 0000"/>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleAddress">Name on Card</Label>
-        <Input type="text" name="address" id="exampleAddress" placeholder="Bayu Lestiawan"/>
-      </FormGroup>
-      <Row form>
-        <Col md={6}>
-          <FormGroup>
-            <Label for="exampleCardExpired">Expired Date</Label>
-            <Input type="text" name="text" id="exampleCardExpired" placeholder="MM/YY" />
-          </FormGroup>
-        </Col>
-        <Col md={6}>
-          <FormGroup>
-            <Label for="exampleCVC">CVC</Label>
-            <Input type="text" name="text" id="exampleCVC" placeholder="123" />
-          </FormGroup>
-        </Col>
-      </Row>
-      <Button>Submit</Button>
-    </Form>
-    </div>
+        <div className="total__payment">
+          <div className="payment__history">
+            <h4>Total Payment</h4>
+          </div>
+          <div className="payment__value">
+            <h4>$450</h4>
+          </div>
+        </div>
+      </div>
+      <div className="container__right">
+      <div class="wrapper">
+            <div class="photo_submit-container">
+              <div class="photo_submit-container">
+                <label class="photo_submit js-photo_submit2">
+                  <input
+                    class="photo_submit-input js-photo_submit-input"
+                    type="file"
+                    accept="image/*"
+                  />
+                  <span class="photo_submit-plus"></span>
+                  <span class="photo_submit-uploadLabel">Upload photo</span>
+                  <span class="photo_submit-delete"></span>
+                </label>
+              </div>
+            </div>
+          </div>
+        <div className="btn-payment">
+        <Button>Pay for $450</Button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default FormPayments;
