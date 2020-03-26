0ea89bb70c2531bad11f82ddee5d313fd7dc1bdf
Bianca Belinda <belindabee42@gmail.com>
2020-03-26 16:09:04 +0700

    personal detail update

--- src/components/PersonalDetail/PersonalDetail.js
+++ src/components/PersonalDetail/PersonalDetail.js
@@ -2,11 +2,9 @@ import React from "react";
 import { Col, Row, Button, Form, FormGroup, Label, Input } from "reactstrap";
 import "../../components/PersonalDetail/persoaldetail.scss";
 import { profileCurrentUser, updateUserDataInvestor } from "../../utils/api";
-import { toast, ToastContainer } from "react-toastify";
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
-
-const MySwal = withReactContent(Swal);
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card } from "reactstrap";
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import classnames from "classnames";
 
 class PersonalDetail extends React.Component {
   constructor(props) {
@@ -17,39 +15,24 @@ class PersonalDetail extends React.Component {
       uploaded_profile_picture: "",
       fullname: "",
       identity_number: "",
-      phone_number: null,
+      phone_number: "",
       country: "",
-      province: "",
+      provice: "",
       city: "",
       zipcode: null,
       address: ""
     };
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      activeTab: "1"
+    };
   }
 
   userData = () => {
-    toast.info("Updating data");
-
-    let formdata = new FormData();
-
-    formdata.append("fullname", this.state.fullname);
-    formdata.append("phone_number", this.state.phone_number);
-    formdata.append("identity_number", this.state.identity_number);
-    formdata.append("photo_profile", this.state.uploaded_profile_picture);
-    formdata.append("country", this.state.country);
-    formdata.append("city", this.state.city);
-    formdata.append("province", this.state.province);
-    formdata.append("postal_code", this.state.zipcode);
-    formdata.append("address", this.state.address);
-
-    updateUserDataInvestor(formdata)
+    alert("OK");
+    updateUserDataInvestor(this.state)
       .then(res => {
-        toast.dismiss();
-
-        MySwal.fire(
-          "Success",
-          "updated user data for: " + this.state.fullname,
-          "success"
-        );
         console.log(res);
       })
       .catch(err => {
@@ -71,11 +54,7 @@ class PersonalDetail extends React.Component {
         profile_picture: res.data.data.profile_picture,
         fullname: res.data.data.fullname,
         phone_number: res.data.data.phone_number,
-        country: res.data.data.country,
-        province: res.data.data.province,
-        city: res.data.data.city,
-        zipcode: res.data.data.postal_code,
-        address: res.data.data.address
+        uploaded_profile_picture: res.data.data.profile_picture
       });
     });
   }
@@ -87,193 +66,232 @@ class PersonalDetail extends React.Component {
     });
   };
 
-  handleProfilePicture = e => {
-    const file = e.target.files;
-
-    this.setState({
-      uploaded_profile_picture: file[0],
-      profile_picture: URL.createObjectURL(file[0])
-    });
-  };
-
-  clearData = () => {
-    this.setState({
-      fullname: "",
-      identity_number: "",
-      phone_number: null,
-      country: "",
-      province: "",
-      city: "",
-      zipcode: null,
-      address: ""
-    });
-  };
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  }
 
   render() {
     return (
-      <div className="container__personaldetail">
-        <ToastContainer />
-        <div className="container__left">
-          <img src={this.state.profile_picture} alt="logo" />
-          <div className="container__content">
-            <h5>Personal Detail</h5>
-            <p>
-              Whether you are an enterpreneur, businessman, employment, or
-              someone with an interest in farm industry.
-            </p>
+      <div>
+
+      {/* HEADER */}
+
+        <div class="parallax-container">
+          <div class="material-parallax">
+            <img src={require("../../asset/image/invest.webp")} alt="logo" />
+          </div>
+          <div class="breadcrumbs-custom-body parallax-content context-dark">
+            <div class="container">
+              <h2 class="breadcrumbs-custom-title">PROFILE DETAIL</h2>
+            </div>
           </div>
         </div>
-        <div className="container__right">
-          <form action="/action_page.php">
-            <img src={this.state.profile_picture} alt="logo" />
-            <input
-              type="file"
-              id="myFile"
-              name="filename"
-              className="input__photo"
-              onChange={e => this.handleProfilePicture(e)}
-            />
-          </form>
-          <Form>
-            <h4>Personal Information</h4>
-            <FormGroup>
-              <Label for="exampleName">FullName</Label>
-              <Input
-                type="text"
-                name="fullname"
-                id="exampleName"
-                placeholder="Name"
-                onChange={e => this.handleFormChange(e)}
-                defaultValue={this.state.fullname}
-              />
-            </FormGroup>
-            {/* <FormGroup>
-              <Label for="identity_number">Identity Number</Label>
-              <Input
-                type="text"
-                name="identity_number"
-                id="identity_number"
-                placeholder="757 000 000 616 0001"
-                onChange={e => this.handleFormChange(e)}
-                defaultValue={this.state.identity_number}
-              />
-            </FormGroup> */}
-            <FormGroup>
-              <Label for="phone_number">Phone Number</Label>
-              <Input
-                type="tel"
-                name="phone_number"
-                id="phone_number"
-                placeholder="+6282194281342"
-                onChange={e => this.handleFormChange(e)}
-                defaultValue={this.state.phone_number}
-              />
-            </FormGroup>
 
-            <div className="contact__information">
-              <h4>Contact Information</h4>
-              <Row form>
-                <Col md={6}>
-                  <FormGroup>
-                    <Label for="country">Country</Label>
-                    <Input
-                      type="text"
-                      name="country"
-                      id="Country"
-                      placeholder="Indonesia"
-                      onChange={e => this.handleFormChange(e)}
-                      defaultValue={this.state.country}
-                    />
-                  </FormGroup>
-                </Col>
-                <Col md={6}>
-                  <FormGroup>
-                    <Label for="Province">Province</Label>
-                    <Input
-                      type="text"
-                      name="province"
-                      id="Province"
-                      placeholder="Jekadaahh"
-                      onChange={e => this.handleFormChange(e)}
-                      defaultValue={this.state.province}
-                    />
-                  </FormGroup>
-                </Col>
-              </Row>
-              <Row form>
-                <Col md={6}>
-                  <FormGroup>
-                    <Label for="City">City</Label>
-                    <Input
-                      type="text"
-                      name="city"
-                      id="City"
-                      placeholder="Jekadahh Timur"
-                      onChange={e => this.handleFormChange(e)}
-                      defaultValue={this.state.city}
-                    />
-                  </FormGroup>
-                </Col>
-                <Col md={6}>
-                  <FormGroup>
-                    <Label for="">Zipcode</Label>
-                    <Input
-                      type="text"
-                      name="zipcode"
-                      id="zipcode"
-                      placeholder="96113"
-                      onChange={e => this.handleFormChange(e)}
-                      defaultValue={this.state.zipcode}
-                    />
-                  </FormGroup>
-                </Col>
-              </Row>
-              <Row form>
-                <Col md={6}>
-                  <FormGroup>
-                    <Label for="Address">Address</Label>
-                    <Input
-                      type="text"
-                      name="address"
-                      id="City"
-                      placeholder="Layangan Putus Street"
-                      onChange={e => this.handleFormChange(e)}
-                      defaultValue={this.state.address}
-                    />
-                  </FormGroup>
-                </Col>
-              </Row>
-            </div>
-            <Row>
-              <Col>
-                <Button
-                  onClick={() => this.userData()}
-                  color="success"
-                  className="btn-block"
-                >
-                  <i className="fa fa-paper-plane"></i> &nbsp; Submit
-                </Button>
-              </Col>
-              <Col>
-                <Button
-                  onClick={() => this.clearData()}
-                  color="danger"
-                  className="btn-block"
-                >
-                  <i className="fa fa-times"></i> &nbsp; Clear
-                </Button>
-              </Col>
-              <Col md="2">
-                <Button title="Refresh page" className="btn-block">
-                  <i
-                    className="fa fa-refresh"
-                    onClick={() => window.location.reload(false)}
-                  ></i>
-                  <span className="d-sm-none"> Reload</span>
-                </Button>
-              </Col>
-            </Row>
-          </Form>
+      {/* SIDEBAR */}
+
+        <div className="container__profile">
+        <Breadcrumb>
+        <BreadcrumbItem><a href="/">Home</a></BreadcrumbItem>
+        <BreadcrumbItem><a href="/profile">Profile</a></BreadcrumbItem>
+        <BreadcrumbItem active>Profile Update</BreadcrumbItem>
+      </Breadcrumb>
+          <h3>Settings</h3>
+          <Row>
+            <Col xs="6" sm="4" md="4">
+              <Nav tabs vertical pills>
+                <div className="profile__pic">
+                  <img
+                    src={require("../../asset/image/user1.jpeg")}
+                    alt="logo"
+                  />
+                  <p> ADITYO S. NENTO</p>
+                </div>
+                <NavItem>
+                  <NavLink
+                    className={classnames({
+                      active: this.state.activeTab === "1"
+                    })}
+                    onClick={() => {
+                      this.toggle("1");
+                    }}
+                  >
+                    Profile
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    className={classnames({
+                      active: this.state.activeTab === "2"
+                    })}
+                    onClick={() => {
+                      this.toggle("2");
+                    }}
+                  >
+                    Investment Status
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Col>
+           
+           
+            <Col xs="16" sm="8" md="8">
+            <div className="container__contentbar">
+              <TabContent activeTab={this.state.activeTab}>
+                <TabPane tabId="1">
+                  <div className="container__photos">
+                    <h4>Update Photo Profile</h4>
+                    <form action="/action_page.php">
+                      <img
+                        src={this.state.uploaded_profile_picture}
+                        alt="logo"
+                      />
+                      <input
+                        type="file"
+                        id="myFile"
+                        name="filename"
+                        className="input__photo"
+                      />
+                    </form>
+                  </div>
+                  <div className="personal__information">
+                    <h4>Personal Information</h4>
+                    <FormGroup>
+                      <Label for="exampleName">FullName</Label>
+                      <Input
+                        type="text"
+                        name="fullname"
+                        id="exampleName"
+                        placeholder="Vicky Hermawan"
+                        onChange={e => this.handleFormChange(e)}
+                        defaultValue={this.state.fullname}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="exampleIdentity">Phone Number</Label>
+                      <Input
+                        type="tel"
+                        name="phone_number"
+                        id="phone_number"
+                        placeholder="+6282194281342"
+                        onChange={e => this.handleFormChange(e)}
+                        defaultValue={this.state.phone_number}
+                      />
+                    </FormGroup>
+
+                    <div className="contact__information">
+                <h4>Contact Information</h4>
+                <Row form>
+                  <Col md={6}>
+                    <FormGroup>
+                      <Label for="exampleCountry">Country</Label>
+                      <Input
+                        type="text"
+                        name="text"
+                        id="Country"
+                        placeholder="Indonesia"
+                        onChange={e => this.handleFormChange(e)}
+                        defaultValue={this.state.country}
+                      />
+                    </FormGroup>
+                  </Col>
+                  <Col md={6}>
+                    <FormGroup>
+                      <Label for="Province">Province</Label>
+                      <Input
+                        type="text"
+                        name="text"
+                        id="Province"
+                        placeholder="Jekadaahh"
+                        onChange={e => this.handleFormChange(e)}
+                        defaultValue={this.state.province}
+                      />
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row form>
+                  <Col md={6}>
+                    <FormGroup>
+                      <Label for="City">City</Label>
+                      <Input
+                        type="text"
+                        name="text"
+                        id="City"
+                        placeholder="Jekadahh Timur"
+                        onChange={e => this.handleFormChange(e)}
+                        defaultValue={this.state.city}
+                      />
+                    </FormGroup>
+                  </Col>
+                  <Col md={6}>
+                    <FormGroup>
+                      <Label for="">Zipcode</Label>
+                      <Input
+                        type="text"
+                        name="text"
+                        id="zipcode"
+                        placeholder="96113"
+                        onChange={e => this.handleFormChange(e)}
+                        defaultValue={this.state.zipcode}
+                      />
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row form>
+                  <Col md={6}>
+                    <FormGroup>
+                      <Label for="Address">Address</Label>
+                      <Input
+                        type="text"
+                        name="text"
+                        id="City"
+                        placeholder="Layangan Putus Street"
+                        onChange={e => this.handleFormChange(e)}
+                        defaultValue={this.state.Address}
+                      />
+                    </FormGroup>
+                    <Button onClick={() => this.userData()}>Submit</Button>
+                  </Col>
+                </Row>
+              </div>
+                  </div>
+                </TabPane>
+                
+              
+                {/*  INVESTMENT STATUS */}
+                
+                <TabPane tabId="2">
+                  <div className="investmentstatus__container">
+                    <div>Product</div>
+                    <div>Payment Amount</div>
+                    <div>Payment Status</div>
+                  </div>
+                  <div className="investmentstatus__content">
+                    <div>Boear Goat</div>
+                    <div>$360</div>
+                    <div className="invest_payoff">Paid off</div>
+                  </div>
+                  <div className="investmentstatus__content">
+                    <div>Boear Goat</div>
+                    <div>$860</div>
+                    <div className="invest_payoffs">Not paid</div>
+                  </div>
+                </TabPane>
+              </TabContent>
+              </div>
+            </Col>
+          </Row>
+        </div>
+
+        <div className="container__personaldetail">
+          <div className="container__right">
+            <Form>
+            
+            </Form>
+          </div>
         </div>
       </div>
     );
