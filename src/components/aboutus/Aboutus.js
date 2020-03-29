b78c34cc063f88eb3c92597a893903ee7bcd0cab
Adityo Nento <adityosnento11@gmail.com>
2020-03-29 15:30:48 +0700

    [Adityo] - Aboutus Progress

--- src/components/aboutus/Aboutus.js
+++ src/components/aboutus/Aboutus.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import Ourteam from './Ourteam'
 import Testimony from './Testimoni'
 import Topcontent from './Topcontent'
+import Infographic from './Infographic'
 
 export default class Aboutus extends Component {
     render() {
         return (
             <div>
                 <Topcontent/>
+                <Infographic/>
                 <Ourteam/>
                 <Testimony/>
             </div>
