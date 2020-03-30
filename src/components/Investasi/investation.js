2f25a7c4309b6dc225a09a70d5d9711479eaddc3
Bianca Belinda <belindabee42@gmail.com>
2020-03-30 23:16:43 +0700

    [Bianca] re-define states and props

--- src/components/Investasi/investation.js
+++ src/components/Investasi/investation.js
@@ -8,83 +8,117 @@ import {
   DropdownItem
 } from "reactstrap";
 import CardComponent from "../home/CardComponent";
+import { liveStockGetAll } from "../../utils/api";
+import { toast, ToastContainer } from "react-toastify";
 
-const Investation = props => {
-  const [dropdownOpen, setOpen] = useState(false);
+class Investation extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const toggle = () => setOpen(!dropdownOpen);
+    this.state = {
+      dropdownOpen: false,
+      livestocks: []
+    };
+  }
 
-  return (
-    <div>
-      <div class="parallax-container">
-        <div class="material-parallax">
-          <img src={require("../../asset/image/invest.webp")} alt="logo" />
+  componentDidMount = props => {
+    liveStockGetAll()
+      .then(res => {
+        console.log(res);
+        this.setState({
+          livestocks: res.data.data.docs,
+          totalPages: res.data.data.totalPages
+        });
+      })
+      .catch(err => {
+        toast.error("Failed to get livestocks data");
+      });
+  };
+
+  toggle = () => {
+    const dropdownOpen = !this.state.dropdownOpen;
+
+    this.setState({
+      dropdownOpen
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ToastContainer />
+        <div class="parallax-container">
+          <div class="material-parallax">
+            <img src={require("../../asset/image/invest.webp")} alt="logo" />
+          </div>
+          <div class="breadcrumbs-custom-body parallax-content context-dark">
+            <div class="container">
+              <h2 class="breadcrumbs-custom-title">INVESTATION LIST</h2>
+            </div>
+          </div>
         </div>
-        <div class="breadcrumbs-custom-body parallax-content context-dark">
-          <div class="container">
-            <h2 class="breadcrumbs-custom-title">INVESTATION LIST</h2>
+        <div className="container__invest">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <a href="/">Home</a>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <a href="/investationlist">Investasi</a>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>Detail</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="category__container">
+            <div className="category__left">
+              <h4>Filter By Category</h4>
+              <div className="category__filter">
+                <input type="checkbox" />
+                <p>All</p>
+              </div>
+              <div className="category__filter">
+                <input type="checkbox" />
+                <p>Cow</p>
+              </div>
+              <div className="category__filter">
+                <input type="checkbox" />
+                <p>Chicken</p>
+              </div>
+              <div className="category__filter">
+                <input type="checkbox" />
+                <p>Goat</p>
+              </div>
+              <div className="category__filter">
+                <input type="checkbox" />
+                <p>Duck</p>
+              </div>
+            </div>
+            <div className="category__right">
+              <div className="category__top">
+                <div>
+                  <p>Showing 1-9 of 10 results</p>
+                </div>
+                <div className="category__sort">
+                  <ButtonDropdown
+                    isOpen={this.statedropdownOpen}
+                    toggle={this.toggle}
+                  >
+                    <DropdownToggle caret size="sm">
+                      Sort by All
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem>Sort by Latest</DropdownItem>
+                      <DropdownItem>Sort by Popularity</DropdownItem>
+                      <DropdownItem>Sort by Alphabet</DropdownItem>
+                    </DropdownMenu>
+                  </ButtonDropdown>
+                </div>
+              </div>
+              <CardComponent livestocks={this.state.livestocks} />
+            </div>
           </div>
         </div>
       </div>
-      <div className="container__invest">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <a href="/">Home</a>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <a href="/investationlist">Investasi</a>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>Detail</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="category__container">
-          <div className="category__left">
-            <h4>Filter By Category</h4>
-            <div className="category__filter">
-              <input type="checkbox" />
-              <p>All</p>
-            </div>
-            <div className="category__filter">
-              <input type="checkbox" />
-              <p>Cow</p>
-            </div>
-            <div className="category__filter">
-              <input type="checkbox" />
-              <p>Chicken</p>
-            </div>
-            <div className="category__filter">
-              <input type="checkbox" />
-              <p>Goat</p>
-            </div>
-            <div className="category__filter">
-              <input type="checkbox" />
-              <p>Duck</p>
-            </div>
-          </div>
-          <div className="category__right">
-            <div className="category__top">
-              <div>
-                <p>Showing 1-9 of 10 results</p>
-              </div>
-              <div className="category__sort">
-                <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
-                  <DropdownToggle caret size="sm">
-                    Sort by All
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem>Sort by Newness</DropdownItem>
-                    <DropdownItem>Sort by Popularity</DropdownItem>
-                    <DropdownItem>Sort by Aplphabet</DropdownItem>
-                  </DropdownMenu>
-                </ButtonDropdown>
-              </div>
-            </div>
-            <CardComponent />
-            <CardComponent />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Investation;
